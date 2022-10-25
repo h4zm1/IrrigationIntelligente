cd4c3cbbed20a38bc6f7c9d7f6cfd4619b520a6f
medaminehssini <medhssinihssini@gmail.com>
2022-10-25 22:08:59 +0100

    add weather api)

--- Backend/Backend/api/urls.py
+++ Backend/Backend/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import include, path
-from .views import InputView
+from .views import InputView, WeatherView
 
 
 urlpatterns = [
     path("input/", InputView.as_view()),
+    path("weather/", WeatherView.as_view()),
 ]
--- Backend/Backend/api/views.py
+++ Backend/Backend/api/views.py
@@ -1,7 +1,12 @@
+from pickletools import long1
+from numpy import dtype
+from regex import D
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializer import InputSerializer
 from .models import Input
+import urllib.request
+import json
 
 
 class InputView(APIView):
@@ -24,3 +29,22 @@ class InputView(APIView):
                 )
             }
         )
+
+
+class WeatherView(APIView):
+    def get(self, request):
+        lon = request.GET.get('lon')
+        lat = request.GET.get('lat')
+        url = urllib.request.urlopen(
+            "https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&units=metric&appid=be5b997fa84e2eee06abbcfc4072f6dd").read()
+
+        convertedData = json.loads(url)
+
+        result = {
+            'city': str(convertedData['name']),
+            "country_code": str(convertedData['sys']['country']),
+            "temp": str(convertedData['main']['temp']),
+            "humidity": str(convertedData['main']['humidity']),
+        }
+
+        return Response(result)
